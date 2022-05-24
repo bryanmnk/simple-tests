ff0a81c2cd37b8163a1893fdf80a9825e457e11a
Bryan Lara <brayan@maniak.com.mx>
2022-05-24 16:53:42 -0500

    Add coverage report

--- jest.config.js
+++ jest.config.js
@@ -17,18 +17,29 @@ module.exports = {
   // clearMocks: false,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
+
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}"
+  ],
+
+  coverageThreshold: {
+    global: {
+      lines: 90,
+      statements: 90
+    }
+  },
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
